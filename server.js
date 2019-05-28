a87bc7a34218661e1b0ef5f21b0bb683feb6d7d1
Jayrald07 <geraldempino@gmail.com>
2019-05-28 06:57:52 -0700

    changed the flow of listening

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res) => {
-	res.sendFile(__dirname+'/public/index.html');
-});
+app.use(express.static('public'));
 
-app.listen(8080,() => {
+app.listen(process.env.PORT || 8080,() => {
 	console.log('Listening Now...');
 })
