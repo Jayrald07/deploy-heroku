ca5e4767b9ac19851a145687e995dd89d739f29d
Jayrald07 <geraldempino@gmail.com>
2019-05-28 21:12:09 -0700

    new flow of program

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app)
+const io = require('socket.io')(http);
 
 app.use(express.static('public'));
+io.on('connection',socket => {
+	socket.on('announce',function(message) {
+		socket.broadcast.emit('broadcastMessage',message);
+	})
+});
 
-app.listen(process.env.PORT || 8080,() => {
+http.listen(process.env.PORT || 5000,() => {
 	console.log('Listening Now...');
 })
